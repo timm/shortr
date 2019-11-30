f0b589b459018a61bc9e9e784af4e20b1abc1547
Tim Menzies <tim.menzies@gmail.com>
2019-11-30 14:04:23 -0500

    saving

--- src/index.lua
+++ src/index.lua
@@ -3,6 +3,8 @@
 --
 -- Just a little light Lua. 
 -- Less is more. Less is best.
+-- 
+-- "He was glad that he liked the country undecorated, hard, and stripped of its finery. He had got down to the bare bones of it, and they were fine and strong and simple." -- [M. Ole](https://www.gutenberg.org/files/289/289-h/289-h.htm#link2H_4_0004)
 --
 -- Useful  for training graduate students in software analytics. "Here, take this executable
 -- specification and code it up in e.g. Python."
