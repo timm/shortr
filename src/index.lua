7b36426e567088afe30e700cb3705f224dc071c3
Tim Menzies <timm@ieee.org>
2019-11-30 15:25:27 -0500

    saving

--- src/index.lua
+++ src/index.lua
@@ -1,8 +1,7 @@
 -- [home](index.html) :: [about](about.html) :: [github](http://github.com/timm/lua) :: [discuss](http://github.com/timm/lua/issues) :: [&copy; 2020](https://github.com/timm/lua/blob/master/LICENSE.md) by [timm](http://menzies.us)
 
 --
--- Just a little light Lua. 
--- Less is more. Less is best.
+-- Just a little light Lua. Less, but butter.
 -- 
 -- "He was glad that he liked the country undecorated, hard, and stripped of its finery. He had got down to the bare bones of it, and they were fine and strong and simple." -- [M. Ole](https://www.gutenberg.org/files/289/289-h/289-h.htm#link2H_4_0004)
 --
