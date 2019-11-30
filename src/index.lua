19578bd77d53ec94252f0cae83ea0f46798a53f4
Tim Menzies <tim.menzies@gmail.com>
2019-11-30 13:57:14 -0500

    saving

--- src/index.lua
+++ src/index.lua
@@ -1,5 +1,9 @@
+-- [home](index.html) :: [about](about.html) :: [github](http://github.com/timm/lua) :: [discuss](http://github.com/timm/lua/issues) :: [&copy; 2020](https://github.com/timm/lua/blob/master/LICENSE.md) by [timm](http://menzies.us)
 
--- <img align=middle width=200 
---  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv_Wnkl9-ffIVj93Ise4d9qijf-8qJNqQZROCY_0Xc4iYvvvG_&s"> 
--- Fork me on [Github](http://github.com/timm/lua).
+--
+-- Just a little light Lua. 
+-- Less is more. Less is best.
+--
+-- Useful  for training graduate students in software analytics. "Here, take this executable
+-- specification and code it up in e.g. Python."
 --
--- src/sym.lua
+++ src/sym.lua
@@ -1,5 +1,7 @@
 -- vim: ts=2 sw=2 sts=2 expandtab:cindent:
 --------- --------- --------- --------- --------- --------- 
+-- [home](index.html) :: [about](about.html) :: [github](http://github.com/timm/lua) :: [discuss](http://github.com/timm/lua/issues) :: [&copy; 2020](https://github.com/timm/lua/blob/master/LICENSE.md) by [timm](http://menzies.us)
+
 
 local Column = require("column")
 local Sym   = {is="Sym"}
