0a43ddcf26f64c834abcee521bff75302d10b892
Tim Menzies <timm@ieee.org>
2019-11-30 02:17:30 -0500

    saving

--- src/index.lua
+++ src/index.lua
@@ -1,2 +1,5 @@
 
+-- <img align=middle width=200 
+--  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv_Wnkl9-ffIVj93Ise4d9qijf-8qJNqQZROCY_0Xc4iYvvvG_&s"> 
 -- Fork me on [Github](http://github.com/timm/lua).
+--
