c20952bf58903f2f72afee7efbea4b164e0f99bf
Tim Menzies <timm@ieee.org>
2019-11-30 01:28:39 -0500

    saving

--- test/num.lua
+++ test/num.lua
@@ -1,6 +1,7 @@
 -- vim: ts=2 sw=2 sts=2 expandtab:cindent:
 --------- --------- --------- --------- --------- --------- 
 
+package.path = '../src/?.lua;' .. package.path
 require "lib"
 
 local ok=require("ok")
