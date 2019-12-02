f6bdb19927f057324e9fcf3bac3eb279d2ea7ff7
Tim Menzies <timm@ieee.org>
2019-12-02 01:01:28 -0500

    saving

--- test/num.lua
+++ test/num.lua
@@ -7,11 +7,6 @@ require "lib"
 local ok=require("ok")
 local Num=require("num")
 
-ok{less=function(   n)
-  n = Num.new{txt = "<newThing" }
-  assert(n.w == -1,"badly weighted")
-end}
-
 ok{sd=function()
   local s1,s2 = Num.new(), Num.new()
   for _,v in pairs{9,2,5,4,12,7,8,11,9,3,
@@ -19,7 +14,6 @@ ok{sd=function()
       Num.add(s1,v) 
       Num.add(s2,v/2) end
   assert(within(3.06,s1.sd,3.07))
-  print(s1.sd, s2.sd)
-  print(Num.norm(s1,2))
-  print(Num.xpect(s1,s2))
+  assert(0.0 == Num.norm(s1,2))
+  assert(within(2.29, Num.xpect(s1,s2), 2.3))
 end}
