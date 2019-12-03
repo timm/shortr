44af7868ea60c8589bca7131c2ba94b0497e15f0
Tim Menzies <tim.menzies@gmail.com>
2019-12-03 14:52:40 -0500

    saving

--- test/all.sh
+++ test/all.sh
@@ -4,8 +4,11 @@ for i in *.lua; do
   s="$s print('\n'..string.rep(\"-\",70)); print('-- $i','\n'); dofile('$i'); "
 done 
 
-lua -e "$s" | 
+/usr/bin/lua -e "$s" | 
+/usr/bin/lua5.3 -e "$s" | 
 gawk ' 1 
        /^-- Test.*oops/ { err = $5}
        END               { exit err - 1}
      '
+
+exit $?
