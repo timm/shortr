08ab8ddf4919fff033bcd6cad8ce52a334edc3c1
Tim Menzies <timm@ieee.org>
2019-12-02 23:03:11 -0500

    saving

--- src/lib.lua
+++ src/lib.lua
@@ -1,10 +1,17 @@
 -- vim: ts=2 sw=2 sts=2 expandtab:cindent:formatoptions+=cro :
 --------- --------- --------- --------- --------- --------- 
 
-local THE=require("the").misc
+-- Set up the standard random seed.
 
+local THE=require("the").misc
 math.randomseed(THE.seed)
+
+-- ------------------------------------
+-- Just some commonly used functions.
+
 r = math.random
+
+function same(x) return x end
 
 function sort(a) table.sort(a) return a end
 
@@ -12,14 +19,14 @@ function within(a,b,c) return b>=a and b<=c end
 
 function round(x) return math.floor( x + 0.5 ) end
 
-function same(x) return x end
-
+-- -------------------------
 -- Convert tables to strings (and do so recursively).
 -- Work over the tables in alphabetically order of the keys.
 -- Skip entries that are _private_ (that start with `_`)
 -- or which point to functions.
 -- If all the indexes are numeric,
 -- then do not show the keys. 
+
 function o(t) print(oo(t))  end
 
 function oo(t,     s,sep,keys, nums)
