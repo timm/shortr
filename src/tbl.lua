bf224b882d9f4efc88b84802b0eabacff9654a14
Tim Menzies <timm@ieee.org>
2019-12-04 00:20:51 -0500

    saving

--- src/tbl.lua
+++ src/tbl.lua
@@ -1,13 +1,18 @@
 -- vim: ts=2 sw=2 sts=2 expandtab:cindent:formatoptions+=cro 
 --------- --------- --------- --------- --------- --------- 
 
+-- <img align=right width=200 
+--  src="https://github.com/timm/lua/raw/master/etc/img/tbl.png">
+-- Tables store [Row](row.html)s, which are summarized
+-- [Columns](columns.html).
+-- Tables can be `fixed` or `frozen` (explained below).
+
+require "lib"
 local Object = require("object")
 local Columns= require("columns")
 local Csv    = require("csv")
 local Row    = require("row")
 local Tbl    = {is="Tbl"}
-
-require "lib"
 
 function Tbl.new(t)
   local i = Object.new()
@@ -20,18 +25,28 @@ function Tbl.new(t)
   return i
 end
 
+-- Tables can be initialized from csv files.
 function Tbl.read(i,file) 
   Csv(file, function(a) Tbl.add(i,a) end) 
   return i end
 
+-- When new data is added to tables,
+-- the first
+-- row define the column name and type (e.g. numeric
+-- or symbolic) and the rest of the rows define the table data.
 function Tbl.add(i, a)
   if #i.cols.all==0 
   then Tbl.header(i,a) 
   else Tbl.row(i,a) end end
 
+-- New headers update the column headers.
 function Tbl.header(i,a) 
   Columns.add(i.cols,a) end
 
+-- New data updates the column summaries
+-- (unless the table is `fixed`) and
+-- to the list of rows in the table
+-- (unless the table is `frozen`).
 function Tbl.row(i,a)
   if not i.frozen then
     for _,c in pairs(i.cols.all) do 
@@ -40,6 +55,10 @@ function Tbl.row(i,a)
   if not i.fixed then
     i.rows[#i.rows + 1] = Row.new(a) end end  
 
+-- New tables can be `cloned` from old ones
+-- by (a) insert all the old column headers into
+-- the new data then (b) optionally, adding in any
+-- some old data.
 function Tbl.clone(i, rows)
    i = Columns.clone(i.columns)
    if rows then 
