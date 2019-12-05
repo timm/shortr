b724b3e0f72f5303867d67acc4b5b7a508d06222
Tim Menzies <timm@ieee.org>
2019-12-05 08:02:37 -0500

    saving

---
+++ src/about.lua
@@ -0,0 +1,9 @@
+-- vim: ts=2 sw=2 sts=2 expandtab:cindent:
+------- --------- --------- --------- --------- --------- 
+
+-- <img align=right  width=200
+--  src="https://github.com/timm/lua/raw/master/etc/img/about.png">
+
+-- - About my [coding conventions](aboutcode.html).
+
+
