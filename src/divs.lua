dbd404fa8b4227642b3326d94e07a593f53a2786
Tim Menzies <timm@ieee.org>
2019-12-13 12:24:04 -0500

    saving

--- src/divs.lua
+++ src/divs.lua
@@ -1,19 +1,34 @@
 -- vim: ts=2 sw=2 sts=2 expandtab:cindent:
 --------- --------- --------- --------- --------- --------- 
 
+-- Simple, fast,  recursive discretizer. Sorts all the 
+-- numbers once, then estimates standard deviation for any
+-- sub range using the 90th-10th percentile difference within
+-- that sub-range.
+
 local Lib=require("lib")
 local r, o, round, abs = Lib.r, Lib.o, Lib.round, Lib.abs
 
+-- -------
+-- Magic parameter. 
+-- Set using [this code](https://gist.github.com/timm/934d4664de105544e51cc67444aa8c60).
+-- Standard deviation of a sub-range estiamted via `(90th-10th)/magic`.
+
+local magic=2.56
+
+-- --------------
+-- The worker
 local   function divs (a,t)
-  local no,max,f,step,stop,start, epsilon, trivial,cohen
-  local out,a1, step, stop, start -- local variables
+  local no,max,f,step,stop,start, epsilon, trivial,cohen -- config
+  local out,a1, step, stop, start -- local vars
   local x,p,mid,var,expect,argmin,recurse -- local functions
   -------------------------------------
   -- Support
   function x(z)      return a1[math.floor(z)] end
   function p(z)      return x(z*#a1 ) end
   function mid(i,j)  return x(i + .5*(j-i) ) end
-  function var(i,j)  return abs((x(i+.9*(j-i)) - x(i+.1*(j-i)))/2.7) end
+  function var(i,j)  
+    return abs((x(i+.9*(j-i)) - x(i+.1*(j-i)))/magic) end
   function xpect(i,m,j)
     local n=j-i+1
     return (m-i)/n*var(i,m) + (j-m -1)/n*var(m+1,j) end
