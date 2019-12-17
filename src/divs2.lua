f163ae149ae8610eb16ff0f75be5d3cfb8a1364c
Tim Menzies <timm@ieee.org>
2019-12-17 00:12:21 -0500

    saving

--- src/divs2.lua
+++ src/divs2.lua
@@ -82,16 +82,19 @@ end
 -- As we recurse, take the stats the upper level
 -- and split those into `x,y` values, left and right 
 -- of the split (into the lsts `xl,yl, xr,yr`).
--- For each split, create a record `{x=..,yvar=..}`
--- showing a summary of the `x ` and `y` values in this split.
 local function recurse(i,a,lo,hi,xall,yall,out,depth,x0,yvar)
   x0   = copy(xall) 
   yvar = i.ytype.var(yall)
   local cut,lx,ly, rx,ry = argmin(i,a,lo, hi,xall,yall)
   if   cut and depth > 0
-  then recurse(i,a,lo,    cut, lx,ly, out, depth-1)
+  then 
+       -- If we can find somewhere to split, then recurse.
+       recurse(i,a,lo,    cut, lx,ly, out, depth-1)
        recurse(i,a,cut+1, hi,  rx,ry, out, depth-1) 
   else 
+       -- If no new split, then add all of `lo` to `hi`
+       -- into the `out` list. Create a record `{x=..,yvar=..}`
+       -- showing a summary of the `x ` and `y` values in this region.
        x0.lo = i.fx(a[lo])
        x0.hi = i.fx(a[hi])
        out[ #out+1 ] = {x=x0, yvar=yvar} end
