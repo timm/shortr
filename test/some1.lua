48eee57d9b51533230261960e0c0c73f6932009d
Tim Menzies <tim.menzies@gmail.com>
2019-12-20 23:34:05 -0500

    saving

--- test/some1.lua
+++ test/some1.lua
@@ -24,34 +24,48 @@ local function w4(   k1,k2,k3,k4)
   return function(x) 
      return w(x,1,k1) + w(x,1,k2) + w(x,1,k3) + w(x,1,k4) end 
 end
-
-all = {}
-m   = 128
-s0  = Some0.new{most=m}
-s1  = Some1.new{most=m}
-f   = w4()
-for i=1,100 do
-  for x=0,2.5,.01 do 
-    local z = f(x) 
-    Some0.add(s0,z)
-    Some1.add(s1,z)
-    all[#all+1] = z end end
-
 function p(n,a) return a[math.floor(#a*n)] end
 
 function r4(x) local n=10^4; return math.floor(n*x)/n end
 function p4(x) return 100 *r4(x) end
 
-table.sort(all)
-print(0,s0.n, #(s0.has))
-print(1,s1.n, #(s1._has))
-table.sort(s0.has)
-for _,j in pairs{.1,.2,.3,.4,.5,.6,.7,.8,.9,1} do 
-  print(j, r4(p(j,all)), 
-           r4(p(j,s0.has)),
-           r4(p(j,Some1.has(s1))),
-           p4(abs(p(j,s0.has)-p(j,all))/ p(j,all)),
-           p4(abs(p(j,Some1.has(s1))-p(j,all))/ p(j,all)))
+function go(ii,m)
+  local all = {}
+  m   = m or 256 -- keep this above 255. but it doesn't seem to matter how high we go
+  local s0  = Some0.new{most=m}
+  local s1  = Some1.new{most=m}
+  local f   = w4()
+  for i=1,ii do
+    for x=0,2.5,.01 do 
+      local z = f(x) 
+      Some0.add(s0,z)
+      Some1.add(s1,z)
+      all[#all+1] = z end end
+
+  table.sort(all)
+  table.sort(s0.has)
+  local e0,e1=0,0
+  local e0s,e1s=0,0
+  local r=0
+  for _,j in pairs{.1,.2,.3,.4,.5,.6,.7,.8,.9} do 
+        e0 = abs(p(j,s0.has)-p(j,all))/ p(j,all)
+        e1= abs(p(j,Some1.has(s1))-p(j,all))/ p(j,all)
+        e0s = e0s+e0
+        e1s = e1s+e1
+        r=r+1
+        --print(j, r4(p(j,all)), r4(p(j,s0.has)), r4(p(j,Some1.has(s1))), p4(e0), p4(e1))
+  end
+  print(ii,m, p4(e0s/r), p4(e1s/r))
+end
+
+for ii =1.1,10 do
+print("")
+go(ii,32)
+go(ii,64)
+go(ii,128)
+go(ii,256)
+go(ii,512)
+go(ii,1024)
 end
 
 same{adds100=function(   s,d)
