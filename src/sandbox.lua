e87e4f8e4ba6f3b1f8fcc0fb3f2fce6e33c06a0a
Tim Menzies <timm@ieee.org>
2022-03-02 01:50:16 -0500

    save

--- src/sandbox.lua
+++ src/sandbox.lua
@@ -1,14 +1,14 @@
-function bsearch(a,want,firstp,  lo,mid,hi,out)
-  out, lo, hi = 1, 1,  #a
+function bsearch(t,want,firstp,  lo,mid,hi,out)
+  out, lo, hi = 1, 1, #t
   while lo <= hi do
     mid = (lo + hi)//2;
-    if want == a[mid] then 
+    if want == t[mid] then 
       out = mid
       if firstp then hi=mid-1 else lo=mid+1 end
     else 
-      if want < a[mid] then hi=mid-1 else lo=mid+1 end end end
+      if want < t[mid] then hi=mid-1 else lo=mid+1 end end end
   return out end 
 
-local key,t = 40,{10,20,20,20,40,50}
+local key,t = 20,{10,20,20,20,40,50}
 print(bsearch(t,key,true))
 print(bsearch(t,key,false))
