bcdcbf13c37faefad75a1ba7f4214e3b6018e846
Tim Menzies <timm@ieee.org>
2022-03-31 00:05:36 -0400

    save

--- src/point.lua
+++ src/point.lua
@@ -42,23 +42,31 @@ local Num = class("Num", Col)
 function Num:new(at,name)
   self:super(at,name)
   self.w = self.name:find"-$" and -1 or 1
-  self.ok, self.has  = true,{}
+  self.ok, self._has  = true,{}
   self.max= 64
   self.lo,self.hi = math.huge,-math.huge end
 
 function Num:add1(x,inc)
   self.hi = math.max(x, self.hi)
   self.lo = math.min(x, self.lo)
-  local a = self.has
+  local a = self._has
   if     #a  < self.max        then self.ok=false; a[1+#a]         =x 
   elseif r() < self.max/self.n then self.ok=false; a[1+(r()*#a)//1] =x end end
 
-function Num:all()
-  if not self.ok then self.ok=true; table.sort(self.has) end
-  return self.has end
+function Num:has()
+  if not self.ok then self.ok=true; table.sort(self._has) end
+  return self._has end
 
-function Num:mid()   return per(self.all(), .5) end
-function Num:sd(  a) a=self:all(); return (per(a,.9) - per(a..1))/2.54 end
+function Num:mid()   return per(self:has(), .5) end
+function Num:div(  a) a=self:has(); return (per(a,.9) - per(a..1))/2.54 end
+
+function Num:same(x,y) return math.abs(x-y) <= self:div()*.35 end
+
+function Num:merge(other,   out)
+  out = Num(self.at, self.name)
+  for _,n in pairs(self._has)  do out:add(n) end
+  for _,n in pairs(other._has) do out:add(n) end
+  return out end
 
 ---      _      ._ _  
 ---     _>  \/  | | | 
@@ -67,19 +75,29 @@ function Num:sd(  a) a=self:all(); return (per(a,.9) - per(a..1))/2.54 end
 local Sym = class("Sym", Col) 
 function Sym:new(at,name)
   self:super(at,name)
-  self._all = {}
+  self.has = {}
   self.mode,self.most = nil,0 end
 
 function Sym:add1(x,inc)
-  self._all[x] = (self._all[x] or 0) + inc
-  if self._all[x] > self.most then
-    self.most, self.mode = self._all[x], x end end
+  self.has[x] = (self.has[x] or 0) + inc
+  if self.has[x] > self.most then
+    self.most, self.mode = self.has[x], x end end
+
+function Sym:mid() return self.mode end
+function Sym:div(  e,p) 
+  e=0; for _,v in pairs(self.has) do p=v/self.n; e = e - p*math.log(p,2) end
+  return e end
+
+function Sym:merge(other,   out)
+  out = Sym(self.at, self.name)
+  for x,n in pairs(self.has)  do out:add(x,n) end
+  for x,n in pairs(other.has) do out:add(x,n) end
+  return out end
 
 print(Sym(23,"thing"):adds{"a","a","b"})
 local n = Num(23,"thing")
 for i=1,1000 do n:add(i) end
 
-
-for i,x in pairs(n:all()) do io.write(x," ") end
+for i,x in pairs(n:has()) do io.write(x," ") end
 
 for k,v in pairs(_ENV) do if not b4[k] then print("?",k,type(v)) end end
