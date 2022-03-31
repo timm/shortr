a3585343d1b744f34e08763423300964c41cb3ed
Tim Menzies <timm@ieee.org>
2022-03-30 23:40:14 -0400

    save

--- src/point.lua
+++ src/point.lua
@@ -1,32 +1,13 @@
 local b4={}; for k,_ in pairs(_ENV) do b4[k]=k end 
 local class = require"class"
-local the,help = {},[[
 
-brknbad: explore the world better, explore the world for good.
-(c) 2022, Tim Menzies
-
-     .-------.  
-     | Ba    | Bad <----.  planning= (better - bad)
-     |    56 |          |  monitor = (bad - better)
-     .-------.------.   |  
-             | Be   |   v  
-             |    4 | Better  
-             .------.  
-
-USAGE:
-  ./bnb [OPTIONS]
-
-OPTIONS:
-  -K       -K  manage low class counts     = 1
-  -file    -f  file name                   = ../etc/data/auto93.csv
-  -help    -h  show help                   = false
-  -bins    -b  bins                        = 16 
-  -todo    -t  start up action             = nothing
-  -wait    -w  wait                        = 10]]
+local r=math.random
+local function per(a,p) return a[1+((p or .5)*#a)//1] end
 
 local Obj=class("Obj")
+
 function Obj:__tostring()
-  local s,sep,tmp,v="","",{}
+  local s,sep,tmp,v = "","",{}
   for k,_ in pairs(self) do 
     if tostring(k):sub(1,1) ~= "_" then tmp[1+#tmp]= k end end
   table.sort(tmp)
@@ -35,62 +16,6 @@ function Obj:__tostring()
     s = s .. sep .. string.format(":%s %s",k,v)
     sep=" " end
   return (self._is or "").."{"..s.."}" end
-
-local function push(t,a) t[1+#t]=a; return a end 
-local function push(t,a) t[1+#t]=a; return a end 
-local function last(t)   return t[#t] end
-local function coerce(x) 
-  return type(x)~="string" and x or math.tointeger(x) or tonumber(x) or x end
-
----     ._ _    _.  o  ._  
----     | | |  (_|  |  | | 
-
-local Main=class("Main")
-function Main:new()
-  self.fails= 0
-  self.used = {}
-  self.seek = "\n  ([-]([^%s]+))[%s]+(-[^%s]+)[^\n]*%s([^%s]+)" end
-
-function Main:add(help)
-  local d = {}
-  help:gsub(self.seek,
-    function(long,key,short,x)
-      assert(not self.used[short], "repeated short flag ["..short.."]")
-      self.used[short]=short
-      for n,flag in ipairs(arg) do 
-        if flag==short or flag==long then
-          x = x=="false" and true or x=="true" and "false" or arg[n+1] end end 
-      d[key] = coerce(x) end)
-  if d.help then os.exit(print(help)) end
-  return d end
-
--- function bsearch(t,want,firstp,  lo,mid,hi,out)
---   out, lo, hi = 1, lo or 1, #t
---   while lo <= hi do
---     mid = (lo + hi)//2;
---     if want == t[mid] then 
---       out = mid
---       if firstp then hi=mid-1 else lo=mid+1 end
---     else 
---       if want < t[mid] then hi=mid-1 else lo=mid+1 end end end
---   return out end 
---
--- local key,t = 20,{10,20,20,20,40,50}
---
---
--- lo = bsearch(t,key,true)
--- hi = bsearch(t,key,false,lo)
--- print(lo,hi)
---
-local Bins = class("Bins", Obj)
-function Bins:new(lo,hi) 
-  self.all, gap = {}, (hi-lo)/the.bins
-  b4 = -math.huge
-  for j=lo,hi,gap do b4 = push(self.all,  {lo=b4, hi=j, hi=hi, seen={}}).hi end 
-  last.hi = math.huge end 
-
----      _   _   | 
----     (_  (_)  | 
 
 local Col = class("Col", Obj)
 function Col:new(at,name)
@@ -105,6 +30,11 @@ function Col:add(x,inc)
   if x ~= "?" then inc=inc or 1; self.n = self.n + inc; self:add1(x,inc) end 
   return x end
 
+function Col:merged(other,  out)
+  out = self:merge(other)
+  if out:div()*.95 <= (sellf.n*self:div() + other.n*other:div())/out.n then
+    return out end end
+
 ---     ._        ._ _  
 ---     | |  |_|  | | | 
 
@@ -112,13 +42,24 @@ local Num = class("Num", Col)
 function Num:new(at,name)
   self:super(at,name)
   self.w = self.name:find"-$" and -1 or 1
-  self.bins = {}
-  self.mu,self.sd,self.lo,self.hi = 0,0,math.huge,-math.huge end
+  self.ok, self.has  = true,{}
+  self.max= 64
+  self.lo,self.hi = math.huge,-math.huge end
 
 function Num:add1(x,inc)
   self.hi = math.max(x, self.hi)
   self.lo = math.min(x, self.lo)
-  self.mu = self.mu + (x - self.mu)/self.n end
+  local a = self.has
+  if     #a  < self.max        then self.ok=false; a[1+#a]         =x 
+  elseif r() < self.max/self.n then self.ok=false; a[1+(r()*#a)//1] =x end end
+
+function Num:all()
+  if not self.ok then self.ok=true; table.sort(self.has) end
+  return self.has end
+
+function Num:mid()   return per(self.all(), .5) end
+function Num:sd(  a) a=self:all(); return (per(a,.9) - per(a..1))/2.54 end
+
 ---      _      ._ _  
 ---     _>  \/  | | | 
 ---         /         
@@ -134,6 +75,11 @@ function Sym:add1(x,inc)
   if self._all[x] > self.most then
     self.most, self.mode = self._all[x], x end end
 
-print(Num(23,"thing"):adds{100,200,300})
 print(Sym(23,"thing"):adds{"a","a","b"})
+local n = Num(23,"thing")
+for i=1,1000 do n:add(i) end
+
+
+for i,x in pairs(n:all()) do io.write(x," ") end
+
 for k,v in pairs(_ENV) do if not b4[k] then print("?",k,type(v)) end end
