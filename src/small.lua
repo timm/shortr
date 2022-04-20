336aef5ff7271750157c6c6e8171c910117b71ae
Tim Menzies <timm@ieee.org>
2022-04-20 00:18:41 -0400

    save

--- src/small.lua
+++ src/small.lua
@@ -1,7 +1,8 @@
 local b4={}; for k,v in pairs(_ENV) do b4[k]=v end
-local coerce, csv,lt,obj,push,settings,o,oo,sort,the
+local any,coerce,csv,fails,fmt,go,id,lt,many,map,obj,push
+local no,o,oo,ok,per,r,rnd,rnds,sort,sum,the,work1,work
 local the,help={},[[ 
-brknbad: explore the world better, explore the world for good.
+small: explore the world better, explore the world for good.
 (c) 2022, Tim Menzies
 
      .-------.  
@@ -29,39 +30,74 @@ OPTIONS:
   -leaves  -l  number of items in leaves   = .5
   -p       -p  coefficient on distance     = 2
   -rest    -R  rest is -R*best             = 4
+  -rnd     -r  rounding numbers            = %5.3f
   -seed    -S  seed                        = 10019
   -some    -s  sample size for distances   = 512
   -todo    -t  start up action             = nothing
   -wait    -w  wait                        = 10]]
---------------------------------------------------------------------------------     
+
+--------------------------------------------------------------------------------  
+r = math.random
+fmt = string.format
+function lt(x) return function(t,u) return t[x] < u[x] end end
+function sort(t,f) table.sort(t,type(f)=="string" and lt(f) or f);return t end
+
+function push(t,x)   t[1+#t]=x; return x end
+function map(t,f, u) u={};for _,v in pairs(t) do u[1+#u]=f(v) end;return u end
+function sum(t,f, u) u=0; for _,v in pairs(t) do u=u+f(v)     end;return u end
+
+function any(a) return a[r()*(#a)//1] end
+function many(a,n,  u) u={};for j=1,n do push(u,any(a)) end;return u end
+
+function per(t,p) return t[ ((p or .5)*#t) // 1 ] end 
+
+function oo(t) print(o(t)) end
+
+function rnds(t,f) return map(t, function(x) return rnd(x,f) end) end
+function rnd(x,f) 
+  return fmt(type(x)=="number" and (x~=x//1 and f or the.rnd) or"%s",x) end
+
+function o(t,    u,one)
+  one= function(k,v) return #t>0 and tostring(v) or fmt(":%s %s",k,v) end
+  u={}; for k,v in pairs(t) do u[1+#u] = one(k,v) end
+  if #t==0 then sort(u) end
+  return (t.is or "").."{"..table.concat(u," ").."}" end
+
 function coerce(x)
   x = x:match"^%s*(.-)%s*$"
   if x=="true" then return true elseif x=="false" then return false end
   return math.tointeger(x) or tonumber(x) or x end
 
-help:gsub("\n  ([-]([^%s]+))[%s]+(-[^%s]+)[^\n]*%s([^%s]+)",
-  function(long,key,short,x)
-    for n,flag in ipairs(arg) do 
-      if flag==short or flag==long then
-        x = x=="false" and "true" or x=="true" and "false" or arg[n+1] end end 
-    the[key] = coerce(x) end)
+function csv(src,      cells)
+  function cells(s,  t) 
+    t={}; for y in s:gmatch("([^,]+)") do t[1+#t]=coerce(y) end; return t end
+  src = io.input(src)
+  return function(x) x=io.read()
+    if x then return cells(x) else io.close(src) end end end 
 
-function o(t,u)
-  u={}; 
-  for k,v in pairs(t) do 
-    u[1+#u] = #t>1 and tostring(v) or string.format(":%s %s",k,v) end
-  table.sort(u)
-  return (t.is or "").."{"..table.concat(u," ").."}" end
+function work1(x,    b4)
+  b4={}; for k,v in pairs(the) do b4[k]=v end
+  math.randomseed(the.seed)
+  if go[x] then print(x); go[x]() end 
+  for k,v in pairs(b4) do the[k]=v end end
 
-function obj(name,    t,new)
+function work(   t)
+  t={}; for k,_ in pairs(go) do push(t,k) end
+  for _,x in pairs(sort(t)) do work1(x) end  end
+
+------------------------------------------------------------------------------
+local _id=0
+function id() _id = _id+1; return _id end
+
+function obj(name,    t,new,str)
   function new(kl,...) 
-    local obj=setmetatable({},kl); kl.new(obj,...); return obj end 
-  t={__tostring=o, is=name or ""}; t.__index=t
-  return setmetatable(t, {__call=new}) end
---------------------------------------------------------------------------------     
+    local x=setmetatable({id=id()},kl); kl.new(x,...); return x end 
+  t = {__tostring=o, is=name or ""}; t.__index=t
+  return setmetatable(t, {__call=new}) end
+
+-------------------------------------------------------------------------------
 local Num=obj"Num"
 function Num:new(at,txt) 
-   oo{new=true,at=at,txt=txt}
    self.at  = at or 0
    self.txt = txt or ""
    self.n, self.mu, self.m2 = 0,0,0
@@ -78,16 +114,23 @@ function Num:add(x,        d)
     self.m2 = self.m2 + d*(x - self.mu) end
   return x end
 
-function Num:mid() return x.mu end
+function Num:mid() return self.mu end
 function Num:div() return (self.m2/(self.n - 1))^0.5 end
 
 function Num:norm(x,   lo,hi)
   lo,hi= self.lo, self.hi
   return x=="?" and x or hi-lo < 1E-9 and 0 or (x - lo)/(hi - lo) end 
---------------------------------------------------------------------------------     
+
+function Num:dist(x,y)
+  if     x=="?" and y=="?" then return 1 end
+  if     x=="?"            then y = self:norm(y); x = y<.5 and 1 or 0 
+  elseif y=="?"            then x = self:norm(x); y = x<.5 and 1 or 0
+  else x,y = self:norm(x), self:norm(y) end
+  return math.abs(x - y) end
+
+--------------------------------------------------------------------------------
 local Sym=obj"Sym"
 function Sym:new(at,txt) 
-   oo{at=at,txt=txt}
    self.at  = at or 0
    self.txt = txt or ""
    self.n   = 0
@@ -104,7 +147,10 @@ function Sym:add(x,inc)
 function Sym:mid() return self.mode end
 function Sym:div(  e)
   e=0; for _,v in pairs(t) do e=e-v/self.n*log(v/self.n,2) end; return e end
---------------------------------------------------------------------------------     
+
+function Sym:dist(x,y) return x=="?" and y=="?" and 1 or x==y and 0 or 1 end
+
+--------------------------------------------------------------------------------
 local Cols=obj"Cols"
 function Cols:new(names,    col)
   self.names, self.all, self.x, self.y, self.klass = names, {}, {}, {}, nil
@@ -118,51 +164,121 @@ function Cols:new(names,    col)
 function Cols:add(row)
   for _,col in pairs(self.all) do col:add(row[col.at]) end
   return row end
---------------------------------------------------------------------------------     
+
+--------------------------------------------------------------------------------
+local Row=obj"Row"
+function Row:new(t) self.cells = t end
+
+--------------------------------------------------------------------------------
 local Egs=obj"Egs"
 function Egs:new() self.rows,self.cols = {}, nil end
 
+function Egs:clone(rows,   out)
+  out = Egs():add(self.cols.names)
+  for _,row in pairs(rows or {}) do out:add(row) end 
+  return out end
+
 function Egs:load(file) 
-  for row in csv(file) do oo(row); self:add(row) end; return self end
+  for row in csv(file) do self:add(row) end; return self end
 
 function Egs:add(t)
-  if self.cols then push(self.rows, self.cols:add(t)) else self.cols=Cols(t) end
-  return t end
+  t = t.cells and t.cells or t
+  if   self.cols 
+  then push(self.rows, Row(self.cols:add(t))) 
+  else self.cols=Cols(t) end
+  return self end
 
 function Egs:better(row1,row2)
   local s1, s2, n, e = 0, 0, #self.cols.y, math.exp(1)
   for _,col in pairs(self.cols.y) do
-    print(col)
-    local a = col:norm(row1[col.at])
-    local b = col:norm(row2[col.at])
+    local a = col:norm(row1.cells[col.at])
+    local b = col:norm(row2.cells[col.at])
     s1      = s1 - e^(col.w * (a - b) / n)
     s2      = s2 - e^(col.w * (b - a) / n) end
   return s1 / n < s2 / n  end
 
-function Egs:betters()
-  return sort(self.rows, function(a,b) return self:better(a,b) end)  end
---------------------------------------------------------------------------------     
-function csv(src,      cells)
-  function cells(s,  t) 
-    t={}; for y in s:gmatch("([^,]+)") do t[1+#t]=coerce(y) end; return t end
-  src = io.input(src)
-  return function(x) x=io.read()
-    if x then return cells(x) else io.close(src) end end end 
+function Egs:betters(rows)
+  return sort(rows or self.rows, function(a,b) return self:better(a,b) end) end
 
-function lt(x) return function(t,u) return t[x] < u[x] end end
+function Egs:mid(cols) 
+  return rnds(map(cols or self.cols.y, function(col) return col:mid() end)) end
 
-function oo(t) print(o(t)) end
+function Egs:dist(row1,row2,   d,n)
+  d = sum(self.cols.x, function(col) 
+              return col:dist(row1.cells[col.at], row2.cells[col.at])^the.p end)
+  return (d / (#self.cols.x)) ^ (1/the.p) end
 
-function push(t,x) t[1+#t]=x; return x end
+function Egs:around(row1, rows,    around)
+  function around(row2) return  {dist=self:dist(row1,row2),row=row2} end
+  return sort(map(rows or self.rows,around), lt"dist") end
 
-function sort(t,f) table.sort(t,type(f)=="string" and lt(f) or f); return t end
---------------------------------------------------------------------------------     
-local go,no={},{}
+function Egs:far(row, rows) 
+  return per(self:around(row, rows or many(self.rows, the.some))).row end
+
+function Egs:unsuper(n,     recurse,known,rows,used,rest)
+  function known(row) used[row.id]=true; return row end
+  function recurse(rows,some,x,      y,tmp)
+    if #rows <= 20 then
+      oo(self:clone(rows):mid())
+    else
+      x = known( x or self:far(any(some),some))
+      y = known(      self:far(x,some))
+      if self:better(y, x) then x,y = y,x end
+      tmp = {}
+      for _,row in pairs(rows) do 
+        push(self:dist(row,x) < self:dist(row,y) and tmp or rest, row) end
+      recurse(tmp, many(tmp,n), x)  end
+  end --------------------------
+  used, rest = {}, {}
+  recurse(self.rows, many(self.rows,n)) end
+
+--------------------------------------------------------------------------------
+fails,go,no = 0,{},{}
+function ok(test,msg)
+  print("", test and "PASS "or "FAIL ", msg or "") 
+  if not test then 
+    fails= fails+1 
+    if  the.dump then assert(test,msg) end end end
+
+function go.many()
+  oo(many({10,20,30,40,50,60,70,80,90,100},3)) end
+
+function go.unsuper(   eg)
+  eg = Egs():load(the.file) 
+  for i=1,10 do eg:unsuper(64) end end
+
 function go.eg1(   eg)
-  eg=Egs()
-  print(eg)
-  eg= Egs():load(the.file) end
---------------------------------------------------------------------------------     
-if the.help then os.exit(print(help)) end  
-if arg[1] and go[arg[1]] then go[arg[1]]() end
+  eg = Egs():load(the.file)
+  print(eg.cols.y[1]) end
+
+function go.dist(  eg,row2,t)
+  eg = Egs():load(the.file)
+  t={}; for i=1,20 do 
+    row2= any(eg.rows)
+    push(t, {dist=eg:dist(eg.rows[1],row2), row = row2}) end 
+  oo(eg.rows[1])
+  for _,two in pairs(sort(t,lt"dist")) do oo(two.row.cells) end end
+
+function go.mids( eg,hi,lo,out)
+  eg = Egs():load(the.file)
+  oo(map(eg.cols.y,function(col) return col.txt end))
+  oo(eg:mid()) 
+  lo,hi = eg:clone(), eg:clone()
+  for i,row in pairs(eg.rows) do 
+    if i < 20            then lo:add(row) end
+    if i > #eg.rows - 20 then hi:add(row) end end
+  oo(lo:mid())
+  oo(hi:mid()) end
+
+--------------------------------------------------------------------------------
+help:gsub("\n  ([-]([^%s]+))[%s]+(-[^%s]+)[^\n]*%s([^%s]+)",
+  function(long,key,short,x)
+    for n,flag in ipairs(arg) do 
+      if flag==short or flag==long then
+        x = x=="false" and "true" or x=="true" and "false" or arg[n+1] end end 
+    the[key] = coerce(x) end)
+
+if the.help then print(help) end
+if the.todo=="all" then work() else work1(the.todo) end
 for k,v in pairs(_ENV) do if not b4[k] then print("?",k,type(v)) end end 
+os.exit(fails)
