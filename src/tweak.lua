fbb5897876068658d5aec2b67a1c3c678936248b
Tim Menzies <timm@users.noreply.github.com>
2022-04-26 21:56:33 -0400

    save

--- src/tweak.lua
+++ src/tweak.lua
@@ -10,15 +10,13 @@
 --      \ \ \_ \ \ \_/ \_/ \/\  __/ /\ \L\.\_  \ \ \\`\  
 --       \ \__\ \ \___x___/'\ \____\\ \__/.\_\  \ \_\ \_\
 --        \/__/  \/__//__/   \/____/ \/__/\/_/   \/_/\/_/
-
-local the, help= {}, [[
-tweak: try three weak leaners for multi-objective optimization
+local help= [[
+tweak: tries three weak learners for multi-objective optimization
 (c) 2022, Tim Menzies,  timm@ieee.org, opensource.org/licenses/Fair
 
-learner1: 5 times, discard half the data firthers from best (so 5 evals)
-learner2: classify data according to presence of "best" from learner1
+learner1: n times, discard half the data furthest from best 
+learner2: classify data according to presence of survivors of learner1
 learner3: run learner1 on best "best" found by learner2
-"Best" is defined by Zitler's multi-objective domination predicate. 
 
 USAGE:
   alias twk="lua tweak.lua "
@@ -41,16 +39,17 @@ OPTIONS (other):
   --file    -f  file name                   = ../etc/data/auto93.csv
   --help    -h  show help                   = false
   --rnd     -r  rounding numbers            = %5.3f
-  --go      -g  start up action             = nothing]]
+  --go      -g  start up action             = nothing]] --[[
 
-local Code_Conventions =[[
+ABOUT THE CODE:
 - Settings generated from "help" string
   - Settings can be updated from the strings seed in flags
-  - At start, turn setting strings to real things (via "string2thing")
+  - Settings stored in the global "the"
 
 - Layout code in chunks of size 120 lines (max), broken by line-feed
   - Chunk1=header; Chunk2=utils; Chunk3=objects; Chunk(last)=demos+start-up
 - Layout lines 80 chars side (max)
+  - So use 2 spaces for "tab"
 - Do functions as one-liners (if possible)
 - In order to define code in any order:
   - Near the top, define all function and Object names as "local"
@@ -61,6 +60,7 @@ local Code_Conventions =[[
 - Private object slots (that should not be printed) start with "_".
 - Constructors need not return constructed instance.
 - No inheritance (hard to debug)
+- For code with many parameters, pass in a dictionary with named fields.
 
 - Tests  in the "go" table at end. Reset settings to defaults after each.
 - Tests check for error conditions using "ok" not "assert".
@@ -71,14 +71,9 @@ local Code_Conventions =[[
 - Command line "-S N" sets random seed (so "-S $RANDOM" is "full" random)
 
 - 2nd last line: look for "rogue" globals (there should be none)
-- Last line: exit to operating system with number of failures seen in tests]]
+- Last line: exit to operating system with number of failures seen in tests
 
-help:gsub("\n  ([-][-]([^%s]+))[%s]+(-[^%s]+)[^\n]*%s([^%s]+)",function(f1,k,f2,x)
-  for n,flag in ipairs(arg) do if flag==f1 or flag==f2 then
-    x = x=="false" and"true" or x=="true" and"false" or arg[n+1] end end 
-  the[k] = x end) -- not ready yet. full of strings that need `string2thing
-
-local Details=[[
+ABOUT THE LEARNERS:
 - "mean","mode" are generalized to "mid" (i.e. "mid-point")
 - "standard deviation","entropy" are generalized  to "div" (i.e. "diversity")
 
@@ -93,29 +88,24 @@ local Details=[[
 - "mid" and "div" for EGS are computed recursively by "mid,div" in NUMs,SYMs
 - distances between two rows is computed recursively via "dist" in NUMs,SYMs
 
-- ROW1 is before than ROW2 (i.e. ROW1<ROW2) if its goals dominate (using the
-  Zitzler continuous domination predicate doi.org/10.1145/1830483.1830578
-- ROWs are recursively separated and clustered using random projections
-  implemented via Faloutsos' FASTMAP method: doi.org/10.1145/568271.223812
-- The distance between two ROWs (i.e. ROW1-ROW2) uses Aha's measure for
-  heterogeneous data across the non-goal columns doi.org/10.1007/BF00153759
+- ROW1 before ROW2 (i.e. ROW1<ROW2) if its goals dominate (using [CDOM])
+- ROWs are recursively separated and clustered by [FASTMAP] random projections
+- The distance between two ROWs (i.e. ROW1-ROW2) uses [AHA].
 - ROWs are persistent. They are created once but many be used in many EGS.
 - ROWs have a "_data" pointer where it gets "lo,hi" info needed for distances.
 - For consistency's sake, ROW._data is fixed to the first
-  EGS that holds that row.]]
+  EGS that holds that row.
 
+REFERENCES:
+- [AHA]    : Aha       : doi.org/10.1007/BF00153759
+- [CDOM]   : Zitzler   : doi.org/10.1145/1830483.1830578
+- [FASTMAP]: Faloutsos : doi.org/10.1145/568271.223812      --]]
 ---    _  _ ___ _ _    ____ 
 ---    |  |  |  | |    [__  
 ---    |__|  |  | |___ ___] 
 
-local any,cells,coerce,csv,fmt,goalp,lessp,lt,many,map,median,mode
-local nump,oo,o,obj,per,push,r,rnd,rnds,sort,slice,stats,string2thing
----     ._ _   o   _   _ 
----     | | |  |  _>  (_ 
-
-r = math.random
-function lt(x)        return function(a,b) return a[x] < b[x] end end
-
+local the,any,cells,csv,fmt,fu,lt,many,map = {}
+local oo,o,obj,per,push,R,rnd,rnds,sort,slice,string2thing
 ---                                     _                               
 ---      _  _|_  ._  o  ._    _    _     )    _|_  |_   o  ._    _    _ 
 ---     _>   |_  |   |  | |  (_|  _>    /_     |_  | |  |  | |  (_|  _> 
@@ -125,6 +115,11 @@ function string2thing(x)
   if x=="true" then return true elseif x=="false" then return false end
   return math.tointeger(x) or tonumber(x) or x end
 
+help:gsub("\n  ([-][-]([^%s]+))[%s]+(-[^%s]+)[^\n]*%s([^%s]+)",function(f1,k,f2,x)
+  for n,flag in ipairs(arg) do if flag==f1 or flag==f2 then
+    x = x=="false" and"true" or x=="true" and"false" or arg[n+1] end end 
+  the[k] = string2thing(x) end) 
+
 function csv(src)
   src = io.input(src)
   return function(line, row) 
@@ -133,10 +128,15 @@ function csv(src)
       row={}; for x in line:gmatch("([^,]+)") do row[1+#row]=string2thing(x) end
       return row end end end 
 
+---     ._ _   o   _   _ 
+---     | | |  |  _>  (_ 
+R = math.random
+function fu(x) return function(a)   return a[x] end end
+function lt(x) return function(a,b) return a[x] < b[x] end end
+
 ---     |  o   _  _|_   _ 
 ---     |  |  _>   |_  _> 
-
-function any(a, i)    i=r()*#a//1; i=math.max(1,math.min(i,#a)); return a[i] end
+function any(a, i)    i=R()*#a//1; i=math.max(1,math.min(i,#a)); return a[i] end
 function many(a,n, u) u={}; for j=1,n do        u[1+#u]= any(a) end;return u end
 function map(t,f, u)  u={}; for _,v in pairs(t) do u[1+#u]=f(v) end;return u end
 function per(t,p)     return t[ ((p or .5)*#t) // 1 ] end 
@@ -165,28 +165,17 @@ function rnd(x,f)
 ---     ._    _   |      ._ _    _   ._  ._   |_   o   _  ._ _  
 ---     |_)  (_)  |  \/  | | |  (_)  |   |_)  | |  |  _>  | | | 
 ---     |            /                   |                      
-
-function obj(name,    t,new,str)
+function obj(name,    t,new)
   function new(kl,...) local x=setmetatable({},kl); kl.new(x,...); return x end 
   t = {__tostring=o, is=name or ""}; t.__index=t
-  return setmetatable(t, {__call=new}) end
-
----      _.        _   ._     
----     (_|  |_|  (/_  |   \/ 
----       |                /  
-
-function cells(i,rows,here,there,    n,x)
-  n ,here, there = 0, here or 1, there or #rows
-  return function(   x) while here >= there  do
-                          x = rows[here].cells[here]
-                          here = here+1
-                          if x~= "?" then n=n+1; return n,x end end end end
+  return setmetatable(t, {__call=new}) end
 ---    ____ ___   _ ____ ____ ___ ____ 
 ---    |  | |__]  | |___ |     |  [__  
 ---    |__| |__] _| |___ |___  |  ___] 
 
 local SYM,BIN,NUM,COLS = obj"SYM",obj"BIN",obj"NUM",obj"COLS"
 local ROW,EGS = obj"ROW",obj"EGS"
+
 ---      _      ._ _  
 ---     _>  \/  | | | 
 ---         /         
@@ -194,6 +183,7 @@ function SYM:new(pos,s)
   self.pos, self.txt= pos or 0,s or "" 
   self.n, self.has, self.most, self.mode = 0,{},0,nil end
 
+function SYM:sub(x,inc) return self:add(x, -(inc or 1)) end     
 function SYM:add(x,inc)    
   if x ~= "?" then
     inc = inc or 1
@@ -210,37 +200,18 @@ function SYM:div(   e)
 
 function SYM:dist(x,y) return x=="?" and y=="?" and 1 or x==y and 0 or 1 end
 
-function SYM:bins(rows,     x,n,out,has,tmp,inc)
-  n,out,tmp = 0,{},{}
-  function inc(x) n=n+1; return n end
-  function has(x) tmp[x]=tmp[x] or BIN({txt=self.txt,  pos=self.pos, n=inc(x),
-                                        lo=x ,hi=x, seen={}}) end
-  for _,r in pairs(rows) do 
-    x = r.cells[self.pos]; has(x); push(tmp[x].seen,r.klass) end
-  for _,x in pairs(tmp) do push(out, x) end
+function SYM:bins(rows,     out,known,x)
+  out,known = {},{}
+  for _,row in pairs(rows) do
+    x = row.cells[self.pos]
+    if x~="?" then
+      known[x] = known[x] or push(out, BIN({txt=self.txt,  pos=self.pos,
+                                                lo=x ,hi=x, ys=SYM()})) 
+      known[x].ys:add(row.klass) end end
   return out end
 
----     |_   o  ._  
----     |_)  |  | | 
-
-function BIN:new(t) 
-  self.pos, self.txt, self.n, self.has = t.pos, t.txt, t.n, {}
-  self.lo, self.hi, self.y = t.lo, t.hi, t.y or SYM() end
-
-function BIN:__tostring()
-  local x,lo,hi,big = self.txt, self.lo, self.hi, math.huge
-  if     lo ==  hi  then return fmt("%s == %s",x, lo)  
-  elseif hi ==  big then return fmt("%s >= %s",x, lo)  
-  elseif lo == -big then return fmt("%s < %s", x, hi)  
-  else                   return fmt("%s <= %s < %s",lo,x,hi) end end
-
-function BIN:select(t)
-  t = t.cells and t.cells or t
-  local x, lo, hi = t[self.pos], self.lo, self.hi
-  return x=="?" or lo == hi and lo == x or lo <= x and x < hi end
 ---     ._        ._ _  
 ---     | |  |_|  | | | 
-
 function NUM:new(pos,s) 
   self.pos, self.txt, self.lo, self.hi = pos or 0,s or "",1E32, -1E32
   self.n, self.mu, self.m2 = 0,0,0
@@ -268,7 +239,38 @@ function NUM:dist(x,y)
   if     x=="?"            then y = self:norm(y); x = y<.5 and 1 or 0 
   elseif y=="?"            then x = self:norm(x); y = x<.5 and 1 or 0
   else x,y = self:norm(x), self:norm(y) end
-  return math.abs(x - y) end
+  return math.abs(x - y) end
+
+function NUM:bins(rows,      xy,div,xys,epsilon,small,b4,out)
+  function xy(row, x) 
+    x=row.cells[self.pos]; if x~="?" then return {x=x,y=row.klass} end end
+  function div(lo,hi,        x,y,cut,lhs,rhs,tmp,best,overall)
+    lhs, rhs, overall = SYM(), SYM(), SYM()
+    for i=lo,hi do overall:add( rhs:add(xys[i].y) ) end
+    best = rhs:div()
+    for i=lo,hi do
+      x, y = xys[i].x, xys[i].y
+      lhs:add( rhs:sub( y) )
+      if lhs.n > small and rhs.n > small then
+        if x ~= xys[i+1].x then
+          if x - xys[lo].x > epsilon and xys[hi].x - x > epsilon then
+            tmp = (lhs.n*lhs:div() + rhs.n*rhs:div())  / (lhs.n + rhs.n)
+            if tmp < best then
+              best,cut = tmp,i end end end end end
+    if   cut 
+    then div(lo,    cut)
+         div(cut+1, hi)
+    else b4= push(out, BIN({txt=self.txt, pos=self.pos, lo=b4, 
+                               hi=xys[hi].x, ys=overall})).hi end 
+  end ------------------------------
+  xys     = sort(map(rows,xy), lt"x")
+  b4,out  = -math.huge, {}
+  epsilon = (per(xys,.9).x - per(xys,.1).x) / 2.56*the.cohen
+  small   = (#xys)^the.min
+  div(1, #xys) 
+  out[#out].hi = math.huge
+  return out end
+
 ---      _   _   |   _ 
 ---     (_  (_)  |  _> 
 
@@ -280,9 +282,26 @@ function COLS:new(names,       it,num,sym,col)
       if txt:find"!$" then self.klass = col end
       push(txt:find"[-+!]$" and self.y or self.x, col) end end end
 
+---     |_   o  ._  
+---     |_)  |  | | 
+function BIN:new(t) 
+  self.pos, self.txt  = t.pos, t.txt
+  self.lo, self.hi, self.ys = t.lo, t.hi, t.ys or SYM() end
+
+function BIN:__tostring()
+  local x,lo,hi,big = self.txt, self.lo, self.hi, math.huge
+  if     lo ==  hi  then return fmt("%s == %s",x, lo)  
+  elseif hi ==  big then return fmt("%s >= %s",x, lo)  
+  elseif lo == -big then return fmt("%s < %s", x, hi)  
+  else                   return fmt("%s <= %s < %s",lo,x,hi) end end
+
+function BIN:select(t)
+  t = t.cells and t.cells or t
+  local x, lo, hi = t[self.pos], self.lo, self.hi
+  return x=="?" or lo == hi and lo == x or lo <= x and x < hi end
+
 ---     ._   _        
 ---     |   (_)  \/\/ 
-
 function ROW:new(data,t)
   self._data,self.cells, self.evaluated = data,t, false end
 
@@ -303,9 +322,9 @@ function ROW:__lt(other,   s1,s2,e,y,a,b)
      s2= s2 - e^(col.w * (b - a) / #y) end
   return s1/#y < s2/#y  end
 
----     ._   _          _ 
----     |   (_)  \/\/  _> 
-
+---      _    _    _ 
+---     (/_  (_|  _> 
+---           _|     
 function EGS:new() self.rows,self.cols = {},nil end
 
 function EGS:add(t)
@@ -324,7 +343,7 @@ function EGS:clone(rows, out)
   return out end
 
 function EGS:load(file)
-  for t in csv(the.file) do self:add(t) end 
+  for t in csv(file) do self:add(t) end 
   return self end
 
 function EGS:around(r1,rows,   t)
@@ -337,20 +356,20 @@ function EGS:far(r1,rows)
 function EGS:sway(rows,stop,rest,x,           some,y,c,best,mid)
   rows = rows or self.rows
   stop = stop or 2*the.best*#rows
-  if #rows <= stop then return rows,rest end
   rest = rest or {}
+  if #rows <= stop then return rows,rest end
   some = many(rows,the.some)
   x    = x or self:far(any(some), some)
   y    =      self:far(x,         some)
-  if y < x then x,y = y,x end
+  if y < x then x,y = y,x end -- "x" is now better than "y"
   x.evaluated = true
   y.evaluated = true
   c    = x - y
-  rows = map(t,function(r) return {r=r, x=((r-x)^2+c^2-(r-y)^2)/(2*c)} end) 
+  rows = map(rows,function(r) return {r=r, x=((r-x)^2+c^2-(r-y)^2)/(2*c)} end) 
   best = {}
   mid  = #rows//2
   for i,rx in pairs(sort(rows,lt"x")) do push(i<=mid and best or rest, rx.r) end
-  return self:sway(best,stop,rest,x)  end
+  return self:sway(best,stop,rest,x)  end
 ---    ___  ____ _  _ ____ ____ 
 ---    |  \ |___ |\/| |  | [__  
 ---    |__/ |___ |  | |__| ___] 
@@ -371,14 +390,14 @@ function ok(test,msg)
       fails= fails+1
       if the.dump then assert(test,msg) end end end
 
-function go.rogue( ok)
-  ok={}; for _,k in pairs{ "_G", "_VERSION", "arg", "assert", "collectgarbage",
+function go.rogue( t)
+  t={}; for _,k in pairs{ "_G", "_VERSION", "arg", "assert", "collectgarbage",
   "coroutine", "debug", "dofile", "error", "getmetatable", "io", "ipairs",
   "load", "loadfile", "math", "next", "os", "package", "pairs", "pcall",
   "print", "rawequal", "rawget", "rawlen", "rawset", "require", "select",
   "setmetatable", "string", "table", "tonumber", "tostring", "type", "utf8",
-  "warn", "xpcall"} do ok[k]=true end
-  for k,v in pairs(_ENV) do if not ok[k] then print("?",k, type(v)) end end end
+  "warn", "xpcall"} do t[k]=true end
+  for k,v in pairs(_ENV) do if not t[k] then print("?",k, type(v)) end end end
 
 function go.the()  oo(the) end
 function go.eg( n,out)   
@@ -411,22 +430,47 @@ function go.sort(    egs,rows,n)
   print("rest", o(rnds(egs:clone(slice(rows, n+1 )):mid())))
   end
 
-function go.far(  egs)
+function go.far(  egs,row2)
   egs = EGS():load(the.file)
-  print(egs:far(egs.rows[1])) end
+  row2=egs:far(egs.rows[1]) 
+  print(row2 - egs.rows[1])end
  
 function go.sway(  egs,best,rest)
   egs = EGS():load(the.file)
   best,rest = egs:sway() 
-  end
- 
+  for _,row in pairs(egs.rows) do if row.evaluated then oo(row.cells) end end
+  print("all",  o(rnds(egs:mid())))
+  print("best", o(rnds(egs:clone(best):mid())))
+  print("rest", o(rnds(egs:clone(rest):mid()))) end
 
--------------------------------------------------------------------------------
+function go.symbins( egs)
+  egs = EGS():load(the.file)
+  for i,row in pairs(sort(egs.rows)) do row.klass = i<=#egs.rows//2 end
+  map(egs.cols.x[4]:bins(egs.rows),oo) end
+
+function go.numbins( egs)
+  egs = EGS():load(the.file)
+  for i,row in pairs(sort(egs.rows)) do row.klass = i<=.05*#egs.rows end
+  for _,col in pairs(egs.cols.x) do
+    print(fmt("\n%s",col.txt))
+    map(col:bins(egs.rows),oo) end end
+
+function go.classify(  egs,best,rest)
+  egs = EGS():load(the.file)
+  oo(egs.cols.x[4])
+  best,rest = egs:sway() 
+  for _,row in pairs(egs.rows) do if row.evaluated then oo(row.cells) end end
+  print("all",  o(rnds(egs:mid())))
+  print("best1", o(rnds(egs:clone(best):mid())))
+  print("rest1", o(rnds(egs:clone(rest):mid()))) 
+  for _,row in pairs(best) do row.klass=true end end
+  --for _,row in pairs(many(rest, 3*#best)) do 
+
+ -------------------------------------------------------------------------------
 ---    ____ ___ ____ ____ ___ 
 ---    [__   |  |__| |__/  |  
 ---    ___]  |  |  | |  \  |  
 
-for k,v in pairs(the) do the[k] = string2thing(v) end 
 if the.help then print(help) else main() end
 go.rogue()
 os.exit(fails) 
