a65186e8218db69d9fea30d988897e1a7d709c13
Tim Menzies <timm@users.noreply.github.com>
2022-05-03 11:31:06 -0400

    save

--- src/repgrid.lua
+++ src/repgrid.lua
@@ -18,7 +18,10 @@ OPTIONS (other):
 local rows, aotm = {}
 R=math.random
 
-function atom(x)
+function map(t,f, u)  u={}; for _,v in pairs(t) do u[1+#u]=f(v) end;return u end
+function push(t,x)    t[1+#t]=x; return x end
+
+function thing(x)
   x = x:match"^%s*(.-)%s*$"
   if x=="true" then return true elseif x=="false" then return false end
   return math.tointeger(x) or tonumber(x) or x end
@@ -26,18 +29,15 @@ function atom(x)
 help:gsub("\n  ([-][-]([^%s]+))[%s]+(-[^%s]+)[^\n]*%s([^%s]+)",function(f1,k,f2,x)
   for n,flag in ipairs(arg) do if flag==f1 or flag==f2 then
     x = x=="false" and"true" or x=="true" and"false" or arg[n+1] end end 
-  the[k] = atom(x) end) 
+  the[k] = thing(x) end) 
 
 function csv(f)
   f = io.input(f)
   return function(t, u)
     t=io.read()
     if not t then io.close(f) else
-      u={}; for x in t:gmatch("([^,]+)") do u[1+#u]=atom(x) end
+      u={}; for x in t:gmatch("([^,]+)") do u[1+#u]=thing(x) end
       return u end end end
-
-function map(t,f, u)  u={}; for _,v in pairs(t) do u[1+#u]=f(v) end;return u end
-function push(t,x)    t[1+#t]=x; return x end
 
 function o(t,    u)
   u={}; for k,v in pairs(t) do u[1+#u] = string.format(":%s %s",k,v) end
@@ -48,26 +48,35 @@ function obj(name,    t,new)
     local x=setmetatable({id=id()},kl); kl.new(x,...); return x end 
   t = {__tostring=o, is=name or ""}; t.__index=t
   return setmetatable(t, {__call=new}) end
-
 --------------------------------------------------------------------------------
 local SYM=obj"SYM"
-function SYM:new(i,txt) 
-  self.i, self.txt, self.has,self.bins = i,txt,{},{} end
+function SYM.new(i,at,txt) i.at, i.txt, i.has,i.bins = at,txt,{},{} end
+function SYM.add(i,x)      if x~="?" then i.has[x] = 1+(i.has[x] or 0) end end
+function SYM.addxy(i,x,y)  if x~="?" then i.bins[x] = y+(i.bins[x] or 0) end end
 
-function SYM:add(x) 
-  if x~="?" then self.has[x] = 1+(self.has[x] or 0) end end
+function SYM.mid(i,   m,x)
+  m=0; for y,n in pairs(i.has) do if n>m then m,x=y,n end end; return x end
 
-function SYM:addxy(x,y) 
-  if x~="?" then self.bins[x] = y + (self.bins[x] or 0) end end
-
-function SYM:mid(   m,x)
-  m=0; for y,n in pairs(self.has) do if n>m then m,x=y,n end end; return m end
-
-function SYM:div(   n,e)
-  n=0; for _,m in pairs(self.has) do n = n + m end 
-  e=0; for _,m in pairs(self.has) do e = e - m/n*math.log(m/n,2) end 
+function SYM.div(i,   n,e)
+  n=0; for _,m in pairs(i.has) do n = n + m end 
+  e=0; for _,m in pairs(i.has) do e = e - m/n*math.log(m/n,2) end 
   return e end
+--------------------------------------------------------------------------------
+function BIN.new(i,t) i.pos,i.txt,i.lo,i.hi,i.y = t.pos,t.txt,t,lo,t.hi,t.ys end
+function BIN.of(i,x)  return i.ys.has[x] or 0 end
 
+function BIN.select(i,t,     x)
+  t = t.cells and t.cells or t
+  x = t[i.pos]
+  return x=="?" or i.lo == i.hi and i.lo == x or i.lo <= x and x < i.hi end
+
+function BIN.__tostring(i)
+  local x,lo,hi,big = i.txt, i.lo, i.hi, math.huge
+  if     lo ==  hi  then return fmt("%s == %s",x, lo)  
+  elseif hi ==  big then return fmt("%s >= %s",x, lo)  
+  elseif lo == -big then return fmt("%s < %s", x, hi)  
+  else                   return fmt("%s <= %s < %s",lo,x,hi) end end
+--------------------------------------------------------------------------------
 local NUM=obj"NUM"
 function NUM:new(i,txt) 
   self.i,self.txt, self.lo,self.hi,self.bins = i,txt,math.huge,-math.huge,{} end
