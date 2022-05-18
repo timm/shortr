dabc5c8f3baa4872aa3c990e43951f748b4a6d26
Tim Menzies <timm@ieee.org>
2022-05-18 00:39:54 -0400

    save

--- src/camel.lua
+++ src/camel.lua
@@ -5,8 +5,9 @@ local the = {
              bins  = 16, 
              cohen = .35,
              file  = "../etc/data/auto93.csv",
-             how   = "up",
-             min   = .5
+             how   = "good",
+             min   = .5,
+             sample= 3
             }
 --------------------------------------------------------------------------------
 big =1E32
@@ -64,6 +65,18 @@ function SYM.new(i,at,name)
 
 function SYM.add(i,x)
   if x~="?" then i.n = i.n+1; i.all[x]= 1+(i.all[x] or 0) end end
+
+function SYM.range(i,x) return x end
+
+function SYM.val(u,goal,B,R,how,   b,r,z)
+  local how={
+    good= function(b,r) return ((b<r or b+r < .05) and 0) or b^2/(b+r) end,
+    bad=  function(b,r) return ((r<b or b+r < .05) and 0) or r^2/(b+r) end,
+    novel=function(b,r) return 1/(b+r) end}
+  b, r, z = 0, 0, 1/big
+  for x,n in pairs(i.all) do
+    if x==goal then b=b+n else r=r+n end end
+  return how[the.how or "good"](b/(B+z), r/(R+z)) end
 --------------------------------------------------------------------------------
 function NUM.new(i,at,txt) 
   i.n,i.mu,i.txt,i.at = 0,0,txt or "",at or 0
@@ -79,6 +92,8 @@ function NUM.add(i,x,    d)
 
 function NUM.norm(i,x) 
   return (x=="?" and x) or (i.hi-i.lo<1E-9 and 0) or (x-i.lo)/(i.hi-i.lo) end
+
+function NUM.range(i,x,n,  b) b=(i.hi-i.lo)/n; return math.floor(x/b+0.5)*b end
 --------------------------------------------------------------------------------
 function EGS.new(i,names)
   i.rows, i.names, i.all, i.x, i.y = {}, names, {}, {}, {}
@@ -100,14 +115,28 @@ function EGS.betters(i)
                  return s1/#y < s2/#y end) 
   return i end
 
-function EGS.sorted(i,at)
-  local function as(x) return type(x)=="number" and x or -1E32 end
-  return sort(i.rows,function(a,b) as(a[at], b[at]) end) end
+function EGS.xx1(i,j,y,seen)
+  x=i.rows[j]
+  if x~="?" then 
+    bin= col:range(x)
+    seen[bin] = seen[bin] or {x=NUM(), y=SYM()}
+    seen[bin].x:add(x)
+    seen[bin].y:add(y) end end
 
+function EGS.xx(i)
+  i.rows = i:betters()
+  n1 = (#i.rows)^the.min
+  n2 = (#i.rows - n1)/the.sample*n1
+  for _,col in pairs(i.x) do
+    tmp={}
+    for j=1,n1 do i:xx1(j,true,tmp)
+    for j=n1+1,#i.rows,n2 do i:xx1(j,false,tmp) end
+        
+function RANGE.new(col,lo,    
 function egs(f, i)
   for row in csv(f or the.file) do 
     if i then i:add(row) else i=EGS(row) end end
-  return i:beters() end
+  return i end
 --------------------------------------------------------------------------------
 the = cli(the)
 local x=egs()
