55a0142fc46a6ddd33fc59d71bc9d9349c35f164
Tim Menzies <timm@ieee.org>
2022-05-21 09:20:27 -0400

    save

--- src/chop2.lisp
+++ src/chop2.lisp
@@ -10,6 +10,8 @@
 
 (defvar my (make-settings))
 
+(defun stop () #+sbcl (sb-ext:exit) #+:clisp (ext:exit))
+
 (defmacro aif (test then else)
   `(let (it) (if (setf it ,test) ,then ,else)))
 
@@ -43,53 +45,75 @@
   (if (not (eq it #\?)) it))
 
 (defstruct (num (:constructor %make-num))
-  (at 0) (txt "") (w 1) (n 0) (hi (?ninf my)) (lo (?inf my)))
+  (at 0) (txt "") (w 1) (n 0) (mu 0) (hi (?ninf my)) (lo (?inf my)))
 
 (defun make-num (&key (at 0) (txt ""))
   (%make-num :at at :txt txt :w (if (lessp txt) -1 1)))
 
+(defmethod norm ((self num) x)
+  (with-slots (lo hi) self
+    (if (< (- hi lo) 1E9) (/ (- x lo) (- hi lo)))))
+
 (defmethod add ((self num) x)
-  (with-slots (n lo hi ) self 
+  (with-slots (n lo hi mu) self 
     (when (not (eq x #\?))
       (incf n)
+      (incf mu (/ (- x mu) n))
       (setf lo (min x lo)
             hi (max x hi)))))
 
-(defstruct sym (at 0) (txt "") (n 0) has)
+(defmethod mid ((self num)) (? self mu))
+
+(defstruct sym (at 0) (txt "") (n 0) mode (most 0) has)
 (defmethod add ((self sym) x)
-  (with-slots (n has) self 
+  (with-slots (n has most mode) self 
     (when (not (eq x #\?))
       (incf n)
-      (incf (has x has)))))
+      (incf (has x has))
+      (if (> n most) (setf most n
+                           mode x)))))
+
+(defmethod mid ((self sym)) (? self mode))
 
 (defstruct row has within)
 
-(defstruct (cols (:constructor %make-cols)) names has x y)
+(defmethod lt ((i row) (j row))
+  (let* ((s1 0) (s2 0) 
+         (ys (? i within cols ys))  (n (length ys)))
+    (dolist (y ys (< (/ s1 n) (/ s2 n)))
+      (let ((a (norm y (elt (? i has) (? y at))))
+            (b (norm y (elt (? j has) (? y at)))))
+        (decf s1 (exp (/  (* (? y w) (- a b)) n)))
+        (decf s2 (exp (/  (* (? y w) (- b a)) n)))))))
+
+(defstruct (cols (:constructor %make-cols)) names has xs ys)
 (defun make-cols (lst)
-  (let (has x y (at 0)) 
+  (let (has xs ys (at -1)) 
     (dolist (name lst)
       (let* ((what (if (nump name) 'make-num 'make-sym))
              (col  (funcall what :at (incf at):txt name)))
         (push col has)
         (when (not (skipp name))
-          (if (goalp name) (push col y) (push col x)))))
-    (%make-cols :has (reverse has) :x x :y y :names lst)))
+          (if (goalp name) (push col ys) (push col xs)))))
+    (%make-cols :has (reverse has) :xs xs :ys ys :names lst)))
 
 (defstruct (egs (:constructor %make-egs)) has cols)
 (defun make-egs (&optional src &aux (self (%make-egs)))
-  (cond ((null src)  self)
-        ((consp src) (dolist (row  src self) (add self row)))
-        (t           (make-egs (get-file src)))))
+  (typecase src
+     (null   self)
+     (cons   (dolist (row src self) (add self row)))
+     (symbol (make-egs (get-file src)))))
 
-(defmethod add ((self egs) row)
+(defmethod add ((self egs) (r cons)) (add self (make-row :has r :within self)))
+(defmethod add ((self egs) (r row))
   (with-slots (has cols) self
-    (cond ((consp row) (add self (make-row :has row :within self)))
-          ((null cols) (setf cols (make-cols row)))
-          (t           (mapcar 'add cols (row-has row))
-                       (push row has)))))
+    (cond ((null cols) (setf cols (make-cols (row-has r))))
+          (t           (mapcar 'add (cols-has cols) (row-has r)) 
+                       (push r has)))))
 
 (let ((all (get-file (?file my)))) 
     (make-cols (car all)))
 
-(make-egs (?file my))
-
+(let ((e (make-egs (?file my))))
+  (setf (? e has ) (sort (? e has) #'lt))
+  (dotimes (i 10) (print (? (elt (? e has) i) has))))
