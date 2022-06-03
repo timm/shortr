edad96199b0a346d58d2473610174fbbc3a550b2
Tim Menzies <timm@ieee.org>
2022-06-02 23:25:49 -0400

    save

---
+++ etc/sandbox/tiny.lua
@@ -0,0 +1,10 @@
+rand=math.random
+function bool(x) 
+  if x==nil 
+  then return (rand()<.5 and true or false) 
+  else return type(x)==boolean end end
+
+function bool(x) 
+  if x==nil 
+  then return (rand()<.5 and true or false) 
+  else return type(x)==boolean end end
