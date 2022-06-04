3bc325440872b0ca5219f6be7b702d54ca5c3806
Tim Menzies <timm@ieee.org>
2022-06-04 13:10:28 -0400

    save

--- etc/sandbox/tiny.lua
+++ etc/sandbox/tiny.lua
@@ -1,13 +1,10 @@
---  __                                      ___                         
--- /\ \__    __                            /\_ \                        
--- \ \ ,_\  /\_\     ___     __  __        \//\ \     __  __     __     
---  \ \ \/  \/\ \  /' _ `\  /\ \/\ \         \ \ \   /\ \/\ \  /'__`\   
---   \ \ \_  \ \ \ /\ \/\ \ \ \ \_\ \   __    \_\ \_ \ \ \_\ \/\ \L\.\_ 
---    \ \__\  \ \_\\ \_\ \_\ \/`____ \ /\_\   /\____\ \ \____/\ \__/.\_\
---     \/__/   \/_/ \/_/\/_/  `/___/> \\/_/   \/____/  \/___/  \/__/\/_/
---                               /\___/                                 
---                               \/__/                                  
---
+--   _     _                       _                 
+--  | |_  (_)  _ __    _   _      | |  _   _    __ _ 
+--  | __| | | | '_ \  | | | |     | | | | | |  / _` |
+--  | |_  | | | | | | | |_| |  _  | | | |_| | | (_| |
+--   \__| |_| |_| |_|  \__, | (_) |_|  \__,_|  \__,_|
+--                     |___/                         
+
 local b4={}; for k,_ in pairs(_ENV) do b4[k]=k end 
 local THE,help= {},[[
 TINY:  
@@ -25,21 +22,108 @@ OPTIONS (other):
   --seed  -s  seed          = 10019
   --file  -f  file          = ../../data/auto93.csv]]
 
-local big,cli,copy,csv,demos, fmt,fmtp,map,normpdf
-local oo,pop,push,rand,read,rnd,shuffle,splice,str
-local function is(name,    t,new,x)  
+---------------------------------------------------------------------------
+-- ____ _  _ _  _ ____ ___ _ ____ _  _ ____ 
+-- |___ |  | |\ | |     |  | |  | |\ | [__  
+-- |    |__| | \| |___  |  | |__| | \| ___] 
+--                                          
+local lib={}
+lib.big  = math.huge
+lib.fmt  = string.format
+lib.fmtp = function(...) print(fmt(...)) end 
+lib.rand = math.random
+
+function lib.cli(t)
+  for key,x in pairs(t) do
+    x = lib.str(x)
+    for n,flag in ipairs(arg) do 
+      if flag==("-"..key:sub(1,1)) or flag==("--"..key) then 
+         x= x=="false" and"true" or x=="true" and"false" or arg[n+1] end end 
+    t[key] = lib.read(x) end 
+  return t end
+
+function lib.csv(csvfile) 
+  csvfile = io.input(csvfile)
+  return function(s, t) 
+    s=io.read()
+    if not s then io.close(csvfile) else
+      t={}; for x in s:gmatch("([^,]+)") do t[1+#t] = lib.read(x) end
+      return t end end end 
+
+function lib.demos(THE,go)
+  local fails,backup = 0,{}
+  for k,v in pairs(THE) do backup[k]=v end 
+  for txt,todo in pairs(go[THE.go] and {go[THE.go]} or go) do 
+    for k,v in pairs(backup) do THE[k]=v end 
+    math.randomseed(THE.seed)               
+    io.write(".")
+    local result = todo()
+    if result ~= true then         
+      fails = fails + 1
+      print("--Error",txt,status) end end
+  for k,v in pairs(_ENV) do  if not b4[k] then print("?",k,type(v)) end end
+  os.exit(fails)  end
+
+function lib.copy(t,   u)
+  if type(t) ~= "table" then return t end
+  u={};for k,v in pairs(t) do u[lib.copy(k)]=lib.copy(v) end
+  return setmetatable(u, getmetatable(t)) end
+
+function lib.is(name,    t,new,x)  
   function new(kl,...) x=setmetatable({},kl); kl.new(x,...); return x end 
-  t = {__tostring=str, is=name}; t.__index=t
+  t = {__tostring=lib.str, is=name}; t.__index=t
   return setmetatable(t, {__call=new}) end 
 
-local ROW, ROWS, NUM, SYM = is"ROW", is"ROWS", is"NUM", is"SYM"
+function lib.map(t,f,  u) 
+  u={}; for k,v in pairs(t) do u[1+#u]=(f and f(v) or v) end return u end
 
+function lib.normpdf(x, mu, sd,      denom,nom)
+ return sd==0 and (x==mu and 1 or 0) or
+   math.exp(-1*(x - mu)^2/(2*sd^2)) * 1 / (sd * ((2*math.pi)^0.5)) end
+
+function lib.oo(i) print(lib.str(i)) end
+
+function lib.pop(t)  return table.remove(t) end
+
+function lib.push(t,x) t[1+#t] = x ; return x end
+
+function lib.read(str) 
+  str = str:match"^%s*(.-)%s*$"
+  if str=="true" then return true elseif str=="false" then return false end
+  return math.tointeger(str) or tonumber(str) or str  end
+
+function lib.rnd(n, p) local m=10^(p or 2); return math.floor(n*m+0.5)/m  end
+
+function lib.shuffle(t,    j)
+  for i = #t, 2, -1 do j=math.random(i); t[i], t[j] = t[j], t[i]; end;
+  return t end
+
+function lib.splice( t, i, j, k,    u) 
+  u={};for n=(i or 1)//1,(j or #t)//1,(k or 1)//1 do u[1+#u]=t[n] end;return u end
+
+function lib.str(i,    j) 
+  if type(i)~="table" then return tostring(i) end
+  if #i> 0 then j= lib.map(i,tostring) 
+  else j={}; for k,v in pairs(i) do j[1+#j] = string.format(":%s %s",k,v) end
+       table.sort(j) end
+  return (i.is or "").."{"..table.concat(j," ").."}" end 
 --------------------------------------------------------------------------------
---        _                             
---   __  | |  __ _   ___  ___  ___   ___
---  / _| | | / _` | (_-< (_-< / -_) (_-<
---  \__| |_| \__,_| /__/ /__/ \___| /__/
---                                      
+-- _  _ ____ _  _ ____ ____ 
+-- |\ | |__| |\/| |___ [__  
+-- | \| |  | |  | |___ ___] 
+
+local big,cli,copy,csv,demos  = lib.big, lib.cli, lib.copy,lib.csv,lib.demos
+local fmt,is,map,normpdf,oo   = lib.fmt,  lib.is,lib.map, lib.normpdf,lib.oo
+local normpdf,pop,push,rand   = lib.normpdf, lib.pop,lib.push,lib.rand
+local read,rnd,shuffle,splice = lib.read, lib.rnd, lib.shuffle, lib.splice
+local str                     = lib.str
+
+local ROW, ROWS, NUM, SYM = is"ROW", is"ROWS", is"NUM", is"SYM"
+--------------------------------------------------------------------------------
+-- _  _ ____ ___ _  _ ____ ___  ____ 
+-- |\/| |___  |  |__| |  | |  \ [__  
+-- |  | |___  |  |  | |__| |__/ ___] 
+                                  
 function SYM.new( i, at,txt) 
   i.n, i.at, i.txt = 0, at or 0, txt or ""
   i.has, i.most, i.mode = {}, 0, nil end
@@ -55,7 +139,6 @@ function SYM.like(i,x,prior)
 
 function SYM.mid(i) return i.mode end
 function SYM.dist(i,x,y) return x=="?" and y=="?" and 1 or x==y and 0 or 1 end
-
 -------------------------------------------------------------------------------
 function ROW.new(i,of,cells) i.of, i.cells, i.evaluated = of, cells, true end
 
@@ -77,7 +160,6 @@ function ROW:__sub(other,    cols,d,inc)
     inc = col:dist(self.cells[col.pos], other.cells[col.pos]) 
     d   = d + inc^THE.p end
   return (d / #cols) ^ (1/THE.p) end
-
 -------------------------------------------------------------------------------
 function NUM.new(i, at,txt) 
   i.n, i.at, i.txt = 0, at or 0, txt or ""
@@ -107,7 +189,6 @@ function NUM.dist(i,x,y)
   elseif y=="?"            then x = self:norm(x); y = x<.5 and 1 or 0
   else x,y = self:norm(x), self:norm(y) end
   return math.abs(x - y) end
-
 -------------------------------------------------------------------------------
 function ROWS.new(i,src)
   i.has={}; i.cols={}; i.xs={}; i.ys={}; i.names={}
@@ -130,7 +211,6 @@ function ROWS.add(i,row,   col)
 function ROWS.clone(i,t,    j)
   j=ROWS({i.names}); for _,row in pairs(t or {}) do j:add(row) end; return j end
 
-
 function ROWS.like(i,t,klasses, all,     prior,like,x)
   prior = (#i.has + THE.k) / (all + THE.k * klasses)
   like  = math.log(prior)
@@ -141,95 +221,12 @@ function ROWS.like(i,t,klasses, all,     prior,like,x)
   return like end
 
 function ROWS.mid(i,p,   u) 
-  u={}; for _,col in pairs(i.ys) do u[col.txt] = col:mid() end; return u end
-
----------------------------------------------------------------------------
---    __                      _     _                   
---   / _|  _  _   _ _    __  | |_  (_)  ___   _ _    ___
---  |  _| | || | | ' \  / _| |  _| | | / _ \ | ' \  (_-<
---  |_|    \_,_| |_||_| \__|  \__| |_| \___/ |_||_| /__/
---                                                      
-big  = math.huge
-fmt  = string.format
-fmtp = function(...) print(fmt(...)) end 
-rand = math.random
-
-function cli(t)
-  for key,x in pairs(t) do
-    x=str(x)
-    for n,flag in ipairs(arg) do 
-      if flag==("-"..key:sub(1,1)) or flag==("--"..key) then 
-         x= x=="false" and"true" or x=="true" and"false" or arg[n+1] end end 
-    t[key] = read(x) end 
-  return t end
-
-function csv(csvfile) 
-  csvfile = io.input(csvfile)
-  return function(s, t) 
-    s=io.read()
-    if not s then io.close(csvfile) else
-      t={}; for x in s:gmatch("([^,]+)") do t[1+#t] = read(x) end
-      return t end end end 
-
-function demos(THE,go)
-  local fails,backup = 0,{}
-  for k,v in pairs(THE) do backup[k]=v end 
-  for txt,fun in pairs(go[THE.go] and {go[THE.go]} or go) do 
-    for k,v in pairs(backup) do THE[k]=v end 
-    math.randomseed(THE.seed)               
-    io.write(".")
-    local result = fun()
-    if result ~= true then         
-      fails = fails + 1
-      print("--Error",s,status) end end
-  for k,v in pairs(_ENV) do  if not b4[k] then print("?",k,type(v)) end end
-  os.exit(fails)  end
-
-function copy(t,   u)
-  if type(t) ~= "table" then return t end
-  u={};for k,v in pairs(t) do u[copy(k)]=copy(v) end
-  return setmetatable(u, getmetatable(t)) end
-
-function map(t,f,  u) 
-  u={}; for k,v in pairs(t) do u[1+#u]=(f and f(v) or v) end return u end
-
-function normpdf(x, mu, sd,      denom,nom)
- return sd==0 and (x==mu and 1 or 0 ) or
-   math.exp(-1*(x - mu)^2/(2*sd^2)) * 1 / (sd * ((2*math.pi)^0.5)) end
-
-oo = function(i) print(str(i)) end
-
-function pop(t)  return table.remove(t) end
-
-function push(t,x) t[1+#t] = x ; return x end
-
-function read(str) 
-  str = str:match"^%s*(.-)%s*$"
-  if str=="true" then return true elseif str=="false" then return false end
-  return math.tointeger(str) or tonumber(str) or str  end
-
-function rnd(n, p) local m=10^(p or 2); return math.floor(n*m+0.5)/m  end
-
-function shuffle(t,    j)
-  for i = #t, 2, -1 do j=math.random(i); t[i], t[j] = t[j], t[i]; end;
-  return t end
-
-function splice( t, i, j, k,    u) 
-  u={};for n=(i or 1)//1,(j or #t)//1,(k or 1)//1 do u[1+#u]=t[n] end;return u end
-
-function str(i,    j) 
-  if type(i)~="table" then return tostring(i) end
-  if #i> 0 then j= map(i,tostring) 
-  else j={}; for k,v in pairs(i) do j[1+#j] = string.format(":%s %s",k,v) end
-       table.sort(j) end
-  return (i.is or "").."{"..table.concat(j," ").."}" end 
-
+  u={}; for _,col in pairs(i.ys) do u[col.txt] = col:mid() end; return u end
 ---------------------------------------------------------------------------------
---      _                          
---   __| |  ___   _ __    ___   ___
---  / _` | / -_) | '  \  / _ \ (_-<
---  \__,_| \___| |_|_|_| \___/ /__/
---                                 
+-- ___  ____ _  _ ____ ____ 
+-- |  \ |___ |\/| |  | [__  
+-- |__/ |___ |  | |__| ___] 
+
 local go,no = {},{}
 
 function go.num(  n)
@@ -254,7 +251,7 @@ function go.smo(rows,n,    all,kl,it,most,tmp)
   all  = shuffle(map(rows.has))
   local seen = {pop(all), pop(all), pop(all)} 
   while #seen < 20 and #all > 10 do
-    local n,bests,rests,maybe
+    local n,bests,rests,maybe,good
     table.sort(seen)
     print""
     for _,row in pairs(seen) do io.write(row.rank," ") end; print("")
@@ -275,18 +272,18 @@ function go.smo(rows,n,    all,kl,it,most,tmp)
     push(seen,pop(all))
     table.sort(all, good)
     all = splice(all,1,.66*#all//1)
-  end end
+  end 
+  return true end
  
 ---------------------------------------------------------------------------------
---                  _        
---   _ __    __ _  (_)  _ _  
---  | '  \  / _` | | | | ' \ 
---  |_|_|_| \__,_| |_| |_||_|
-                          
+-- _  _ ____ _ _  _ 
+-- |\/| |__| | |\ | 
+-- |  | |  | | | \| 
+
 help:gsub(" [-][-]([^%s]+)[^\n]*%s([^%s]+)",function(key,x) THE[key]=read(x) end)
 
 if   pcall(debug.getlocal, 4, 1) then 
-  return {ROW=ROW, ROWS=ROWS, NUM=NUM, SYM=SYM, THE=THE} 
+  return {ROW=ROW, ROWS=ROWS, NUM=NUM, SYM=SYM, THE=THE,lib=lib} 
 else 
   THE = cli(THE)
   if THE.help then os.exit(print(help:gsub("[%u][%u%d]+","\27[1;31m%1\27[0m"))) end
