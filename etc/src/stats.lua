206aa435d3e41eaa2095335f2fdd235beb35c933
Tim Menzies <timm@ieee.org>
2022-06-20 19:53:22 -0400

    save

--- etc/src/stats.lua
+++ etc/src/stats.lua
@@ -9,6 +9,8 @@ function kap(t,f,  u) u={}; for k,v in pairs(t) do u[1+#u]=f(k,v) end; return u 
 function maps(t,u,f,  v) v={}; for k,v in pairs(t) do v[1+#v]=f(v,u[k])   end; return v end
 function kaps(t,u,f,  v) v={}; for k,v in pairs(t) do v[1+#v]=f(k,v,u[k]) end; return v end
 
+function per(t,p)     p=p*#t//1; return t[math.max(1,math.min(#t,p))] end
+
 function cat(t,    key,u)
   function key(k,v) if (tostring(k)):sub(1,1)~="_" then return fmt(":%s %s",k,v) end end
   u=  #t>1 and  map(t,f or tostring) or sort(kap(t,key))
@@ -44,15 +46,45 @@ local function _col(i,at,txt)
 local Sym=obj("Sym", _col)
 local Num=obj("Num", function(i,at,txt) _col(i,at,txt); i.nums,i.ok = 256,true end)
 
-function col(at,txt) return (name:find"^[A-Z]" and Num or Sym)(at,txt) end
+--- ### Adding
+function Num.add(i,x,n)
+  if x=="?" then return end
+  n = n or 1
+  for _=1,n  do
+    if   #i.kept < i.nums then i.ok=false;push(i.kept,x) 
+    elseif R() < i.nums/i.n then i.ok=false;i.kept[R(#i.kept)]=x end end end 
 
+function Sym.add(i,x,n)
+  if x=="?" then return end
+  i.ok = false
+  i.kept[x] = n + (i.kept[x] or 0) end 
+
+--- ### Queriying
+function Num.ok(i)
+  if not i.ok then table.sort(i.kept) end
+  i.ok = true
+  return i.kelp end
+
+function Num.mid(i) local a= i:ok(); return per(a,.5) end
+function Sym.mid(i)
+  local mode,most = nil,-1
+  for x,n in pairs(i.kept) do if n > most then most, mode = n, x end end; return mode end
+
+function Num.div(i) local a= i:ok(); return (per(a,.9)-per(a..1))/2.56 end
+function Sym.div(i)
+  local e,log=0, function(x) return math.log(x,2) end
+  for x,n in pairs(i.kept) do if n > 0 then e=e- n/i.n*log(n/i.n) end end
+  return e end
+
+--- ### Column Factory
 local Cols=obj("Cols",function(i,names) 
-  i.x, i.y, i.klass, i.names = {}, {}, nil, names 
-  i.all = kap(names,col)
-  for _,col in pairs(i.all) do
+  i.names, i.x, i.y, i.all,i.klass, i.names = names, {}, {},  {}
+  for at,txt in pairs(names) do
+    col = (name:find"^[A-Z]" and Num or Sym)(at,txt) end
+    push(i.all, col)
     if not col.txt:find":$" then
       push(col.txt:find"[!+-]$" and i.y or i.x, col)
-      if col.txt:find"!$" then i.klass=col end end end end)
+      if col.txt:find"!$" then i.klass=col end end end ) 
 
 function Cols.add(i,row)
   for _,cols in pairs{i.x,i.y} do
