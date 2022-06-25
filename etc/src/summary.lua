4a674ad82c325406a6be023a7259573cae7e7117
Tim Menzies <timm@ieee.org>
2022-06-24 22:10:00 -0400

    save

--- etc/src/summary.lua
+++
@@ -1,9 +0,0 @@
--- ## Update
-local _=require"about"
-local o=require"obj"
-local R,csv,push = _.R, _.csv, _.push
-local COLS,NUM,SOME,SYM = o.COLS, o.NUM, o.SOME, o.SYM
-local ROW,ROWS          = o.ROW, o.ROWS
--- ### add 1 Column
-
-
