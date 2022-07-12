2cd6e887ddc2a83169a7a0fe295a1d57582e6359
Tim Menzies <timm@ieee.org>
2022-07-12 22:26:27 +1000

    save

--- etc/src/oo.lua
+++ etc/src/oo.lua
@@ -1,30 +1,105 @@
-local b4,the,help = {},{},[[
+-- # OO
+-- ## Config
+local b4={}; for k,v in pairs(_ENV) do b4[k]=k end
+local the,help = {},[[
 
-TODO: dont fuse on non-numerics
+oo.lua : stuff that is cool
+(c) 2022 Tim Menzies BSD-two-clause
 
- -c cohen difference in nums    = .35
- -f file  source                = ../../data/auto93.csv
- -g go    action                = help
- -m min   size of small         = .5
- -s seed  random number seed    = 10019]]
+ -c  cohen  difference in nums   = .35
+ -f  file   source               = ../../data/auto93.csv
+ -g  go     action               = help
+ -h  help   show help            = false
+ -m  min    size of small        = .5
+ -s  seed   random number seed   = 10019
+ -S  Some   some items to keep   = 256]]
 
-for k,v in pairs(_ENV) do b4[k]=k end
-local cat,chat,fmt,kap,map
-local new,obj,push,same,showSlots,sort,trim
+local function thing(x) 
+  x = x:match"^%s*(.-)%s*$"
+  if x=="true" then return true elseif x=="false" then return false end
+  return math.tointeger(x) or tonumber(x) or x end 
 
-function obj(txt,base)
-  t={__tostring=showSlots}
+help:gsub("\n [-]%S[%s]+([%S]+)[^\n]+= ([%S]+)",
+          function(k,x) the[k]=thing(x) end)
+
+-- ## Names
+local cat,chat,cli,csv,fmt,kap,lines,map
+local new,obj,per,push,R,rogues,same,sort,trim,words
+
+local _id=0
+function obj(txt,base,  t,new,i)
+  function new(k,...) 
+    _id=_id+1; i=setmetatable({_id=id},k); k.new(i,...); return i end
+  t={__tostring=cat}
   for k,v in pairs(base or {}) do t[k] = v end
   t.is, t.__index =  txt, t
 	return setmetatable(t,{__call=new}) end
 
+-- ## Columns
+-- ### COL
+local COL=obj"COL"
+function COL:new(at,txt)
+  self.at  = at or 0     
+  self.txt = txt or ""  
+  self.n   = 0 end     
+
+function COL:add(x,inc)
+  if x ~= "?" then 
+    inc = inc or 1
+    self.n = self.n + inc
+    self:add1(x,inc) end end
+
+-- ### SOME
+local SOME=obj("SOME",COL)
+function SOME:new(...)
+  COL.new(self, ...)
+  self.kept,self.ok,self.max,self.n = {},true,the.Some,0  end
+
+function SOME:add1(x,inc)
+  for j=1,inc do
+    local a= self.kept
+    if     #a  < self.max        then self.ok=false; push(a,x) 
+    elseif R() < self.max/self.n then self.ok=false; a[R(#a)]=x end end end 
+
+function SOME:has()
+  self.kept = self.ok and self.kept or sort(self.kept)
+  self.ok=true
+  return self.kept  end
+ 
+-- ### NUM
+-- #### Creation
+local NUM=obj("NUM",COL)
+function NUM:new(...)
+  COL.new(self, ...)
+  self.kept = SOME()          
+  self.w = self.txt:find"-$" and -1 or 1 end
+
+-- #### Report
+function NUM.div(i) 
+  local a=i.kept:has(); return (per(a,.9) - per(a,.1))/2.56 end
+
+function NUM.mid(i) 
+  local a=i.kept:has(); return per(a,.5) end
+
+function NUM:norm(x)
+  local a = self.kept:has()
+  return (a[#a]-a[1])<1E-9 or (x-a[1])/(a[#a]-a[1]) end
+
+-- #### Update
+function NUM:add1(x,inc)
+  for j=1,inc do self.kept:add(x) end end 
+
 -- ## Lib
+-- ### Maths
+R=math.random
 -- ### Lists
-function same(x) return x end
+function same(x)      return x end
 function map(t,f,  u) u={};for _,x in pairs(t)do u[1+#u]=f(x) end;return u end
 function kap(t,f,  u) u={};for k,x in pairs(t)do u[1+#u]=f(k,x)end;return u end
 function sort(t,f)    table.sort(t,f); return t end
 function push(t,x)    t[1+#t]=x; return x end
+function per(t,p)     p=p*#t//1; return t[math.max(1,math.min(#t,p))] end
+
 
 -- ### Misc
 function cli(t)
@@ -40,17 +115,6 @@ function rogues()
   for k,v in pairs(_ENV) do if not b4[k] then print("?",k,type(v)) end end end
 
 function same(x) return x end
--- ### Objects
-local _id=0
-function new(k,...) 
-  _id=_id+1;local i=setmetatable({_id=id},k); k.new(i,...); return i end
-
-function showSlots(t,    u,pub)
-  pub = function(k,v) return tostring(k):sub(1,1)~="_" end
-  u={}; for k,v in pairs(t) do if pub(k) then u[1+#u]=(":%s %s"):format(k,v) end end
-  table.sort(u)
-  return (t.is or "").."{"..table.concat(u," ").."}"  end
-
 -- ### String2things
 
 function csv(file,fun)
@@ -62,12 +126,6 @@ function lines(file, fun)
     local line = io.read()
     if not line then return io.close(file) else fun(line) end end end
 
-function thing(x)
-  x=trim(x)
-  if x=="true" then return true elseif x=="false" then return false 
-  else return math.tointeger(x) or tonumber(x) or x end  end
-
-function trim(x) return  x:match"^%s*(.-)%s*$" end
 
 function words(s,sep,fun,      t)
   fun = fun or same
@@ -75,14 +133,19 @@ function words(s,sep,fun,      t)
 
 -- ### Thing2string
 function chat(t) print(cat(t)); return t end
-function cat(t, u) 
-  u={};for k,v in pairs(t) do u[1+#u]=tostring(k) end
-  return "{"..table.concat(u," ").."}" end
+function cat(t,   u,pub) 
+  pub=function(k,v) return tostring(k):sub(1,1)~="_" end
+  if type(t)~="table" then return tostring(t) end
+  if #t>0             then return "{"..table.concat(map(t,tostring)," ").."}" end
+  u={};for k,v in pairs(t) do if pub(k) then u[1+#u]=(":%s %s"):format(k,v) end end
+  table.sort(u)
+  return (t.is or "").."{"..table.concat(u," ").."}"  end
 
 fmt=string.format
 
 -- ### Testing
 local go,fails={},0
+
 function go.all() 
   local defaults={}
   for k,v in pairs(the) do defaults[k]=v end 
@@ -94,11 +157,15 @@ function go.all()
       print("FAIL:",k)
       fails=fails+1 end end end
 
--- ## Demos
+function go.the()  chat(the);    return true end
+function go.some( n) 
+  n = NUM()
+  chat(n)
+  for j=1,10^3 do n:add(j) end
+  chat(n.kept:has()) return true end
 
 -- ## Start
-help:gsub("\n [-]%S[%s]+([%S]+)[^\n]+= ([%S]+)",function(k,x) the[k]=thing(x) end)
 the=cli(the)
-go[the.go]()
+if the.help then print(help) elseif go[the.go] then go[the.go]() end
 rogues()
 os.exit(fails)
