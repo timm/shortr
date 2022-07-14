c1d0de7c32f3e457a806644173c0c3f987035e6b
Tim Menzies <timm@ieee.org>
2022-07-14 16:45:32 +1000

    save

--- etc/src/oo.lua
+++ etc/src/oo.lua
@@ -1,5 +1,5 @@
 -- # [:high_brightness: SHORTr : less (but better) XAI](oo.md)
--- <img align=right width=400 src="xai4.jpeg"> AI and XAI (explainable artificial intelligence) need not be hard. 
+-- <img align=right width=600 src="xai4.jpeg"> AI and XAI (explainable artificial intelligence) need not be hard. 
 -- E.g. here's a few hundred lines of LUA to search N items to find and 
 -- explain the best ones, using just log(N) evals.
 
