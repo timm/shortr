e7e883c2ba2605665267fb65b407f86886a29378
Tim Menzies <timm@ieee.org>
2022-07-22 00:29:56 +1000

    save

--- etc/src/tiyin.lisp
+++ etc/src/tiyin.lisp
@@ -1,3 +1,4 @@
+; vim: et ts=2 tw=2 :
 ; TIYIN (c) 2022, Tim Menzies 
 ; Multi-objective semi-supervised XAI.
 
@@ -12,6 +13,9 @@
     (seed  "-s"  "random number seed"        10019)
     (go    "-g"  "start up action"           ls)))
 
+(defmacro ? (s x &rest xs)
+  (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
+
 (defmethod it (x) x)
 (defmethod it ((x string))
   (let ((y (string-trim '(#\Space #\Tab #\Newline) x)))
@@ -19,7 +23,8 @@
       (let ((z (ignore-errors (read-from-string y))))
         (if (numberp z) z y)))))
 
-(defun cli ( flag help default)
+(defun cli (flag help default)
+  (declare (ignore help))
   (let* ((args #+clisp ext:*args* #+sbcl sb-ext:*posix-argv*)
          (it (member flag args :test 'equal)))
     (cond ((not it) default)
@@ -34,12 +39,24 @@
 (defmacro aif (test then &optional else) `(let ((it ,test)) (if it ,then ,else)))
 
 (with-open-file (stream (my file))
-  (while loop (setf if (read stream nil)) do  (print it)))
+  (loop while (setf if (read stream nil)) do  (print it)))
+
+(defstruct sym (n 0) (txt "") (at 0) has))
+
+(defstruct (num (:constructor %make-num)) (n 0) (txt "") (at 0) has)
+(defun make-num (txt at)
+  (%make-num :txt txt :at at :w (if (find #\- txt) -1 1)))
 
 (defstruct (cols (:constructor %make-cols)) all x y names)
-
-(defun make-cols (lst &aux (self (%make-cols :names lst)))
-   (setf (cols-all self) (mapcar (lambda (x) (make-col x (incf at))))))
-
+(defun make-cols (txts &aux (at -1) (self (%make-cols :txts txts)))
+  (dolist (txt txts self)
+    (let* ((what (if (upper-case-p (char txt 0)) 'make-num 'make-sym))
+           (col  (funcall what  txt  (incf at))
+      (push (? self all) col)
+      (unless (find #\: txt)
+        (if (find #\! txt) (setf (? self y klass) col))
+        (if (or (find #\- txt) (find #\+ txt) (find #\! txt))
+          (push (? self y) col)
+          (push (? self x) col))))))
 
 (print *opt*)
